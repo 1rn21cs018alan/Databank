41c94af699a51dc41f6bda24d0d414552e1b9e06
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-01-17 12:41:09 +0530

    Update Insertions.sql

--- DB/Company/Insertions.sql
+++ DB/Company/Insertions.sql
@@ -20,4 +20,19 @@ insert into Department(dno,dname) values
 ;
 
 insert into Employee(ssn ,	name ,	dno ) values
-	(1003,"Deepak",5);
+	(1003,"Ganesha",5),
+	(1004,"Manish",5),
+	(1005,"Deepak",5),
+	(1006,"Rahul",5),
+	(1007,"Surendra",6),
+	(1008,"Suraj",6),
+	(1009,"Mohandas",6),
+	(1010,"Harsha",2),
+	(1011,"Scott",3),
+	(1012,"Pampers",4),
+	(1013,"Scott",3),
+	(1014,"Scott",3),
+	(1015,"Scott",3),
+	(1016,"Scott",Null),
+	(1017,"Scott",Null)
+;
