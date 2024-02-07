84c94300e434c518003f2835ec48351c76d21b25
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-02-07 13:12:25 +0530

    Create Insertions.sql

---
+++ DB/Ordered/Insertions.sql
@@ -0,0 +1,15 @@
+insert into Salesman values
+	(1,"Arun","bangalore", 0),
+	(2,"Carl","bangalore", 0),
+	(1000,"Ruth","mysore", 0);
+	
+insert into Customer values
+	(1,"Rick"	,"bangalore",10,1	),
+	(2,"Morty"	,"mysore"	,20,1	),
+	(3,"James"	,"bangalore",20,1000);
+	
+insert into Orders values
+	(1,10,'2014-01-01',1,1	),
+	(2,20,'2014-01-01',2,1	),
+	(3,30,'2014-01-02',3,1000),
+	(4,40,'2014-01-02',1,1	);
