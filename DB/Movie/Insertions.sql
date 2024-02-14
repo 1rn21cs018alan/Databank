29094bc0fa95ca863c8bccfff0e78bdbd8c8eb12
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-02-14 12:52:33 +0530

    Create Insertions.sql

---
+++ DB/Movie/Insertions.sql
@@ -0,0 +1,24 @@
+
+insert into Actor values
+	(1,"Arun","M"),
+	(2,"Anu","F");
+	
+insert into Director values
+	(1,"Hitchcock","1234567890"),
+	(2,"Steven Spielberg","0987654321");
+	
+insert into Movie values
+	(1,"Jump",1999,"eng",1),
+	(2,"Leap",2004,"eng",1),
+	(3,"Hop",2020,"eng",2);
+	
+insert into Movie_Cast values
+	(1,1,"main"),
+	(1,2,"side"),
+	(1,3,"side"),
+	(2,1,"main");
+
+insert into Rating values
+	(1,3),
+	(1,5),
+	(2,4);
