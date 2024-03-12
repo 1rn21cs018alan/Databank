15ca76ff9406ae707b115f2a0d27ffd2c72cc15c
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-03-12 18:28:00 +0530

    Update Insertions.sql

--- DB/College/Insertions.sql
+++ DB/College/Insertions.sql
@@ -1,16 +1,16 @@
 
 insert into student values
 	("1rn21cs018","alan","bangalore","0000000000","M"),
-    ("1rn21cs001","aabil","mysure","1111111111","F"),
-    ("1rn20cs002","aadhar","bangalore","2222222222","M"),
-    ("1rn20cs003","aadal","bangalore","3333333333","F"),
-    ("1rn20cs004","adit","bangalore","4444444444","M");
+	("1rn21cs001","aabil","mysure","1111111111","F"),
+	("1rn20cs002","aadhar","bangalore","2222222222","M"),
+	("1rn20cs003","aadal","bangalore","3333333333","F"),
+	("1rn20cs004","adit","bangalore","4444444444","M");
     
 insert into semsec values
 	(1,4,'C'),
-    (2,8,'A'),
-    (3,8,'B'),
-    (4,8,'C');
+	(2,8,'A'),
+	(3,8,'B'),
+	(4,8,'C');
     
 insert into class values
 	("1rn21cs018",1),
