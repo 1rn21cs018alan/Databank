dff6c3ceea5a294f6df3ffd67d51076ed9414497
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-03-12 18:44:30 +0530

    Create Questions.sql

---
+++ DB/College/Questions.sql
@@ -0,0 +1,8 @@
+# 1. List all the student details studying in fourth semester ‘C’ section.
+# 2. Compute the total number of male and female students in each semester and in each section.
+# 3. Create a view of Test1 marks of student USN ‘1RN21CS001’ in all subjects.
+# 4. Calculate the FinalIA (average of best two test marks) and update the corresponding table for all students.
+# 5. Categorize students based on the following criterion: If FinalIA = 17 to 20 then CAT = ‘Outstanding’
+#       If FinalIA = 12 to 16 then CAT = ‘Average’
+#       If FinalIA< 12 then CAT = ‘Weak’
+#       Give these details only for 8th semester A, B, and C section students.
